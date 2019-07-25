316933d68b59a8cb139d1eba2adf3d4461eab952
Johan Thomsen <jth@dbc.dk>
2019-07-25 16:57:09 +0200

    kubernetes: implement the watch-api
    - renamed flag "update-interval" to "reload-every"
    - introduced three new flags "reload-rollup", "disable-watch", "ignore-namespaces"
    
    fixes #22

--- kubernetes/kubernetes.go
+++ kubernetes/kubernetes.go
@@ -7,23 +7,82 @@ import (
 	v13 "k8s.io/api/core/v1"
 	v1beta12 "k8s.io/api/extensions/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	v12 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/kubernetes/typed/extensions/v1beta1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	"math"
 	"net/url"
 	"strconv"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	"sync"
 )
 
+var updateMutex sync.Mutex
+
+
+func WatchBackends(config *util.Config, updateChan chan util.Reload) error {
+	clients, err := GetKubeClient(config.Kubeconfig)
+	if err != nil {
+		return err
+	}
+
+	emptyOptions := v1.ListOptions{}
+
+	ingressWatch, err := clients.ExtensionsV1beta1().Ingresses("").Watch(emptyOptions)
+	if err != nil {
+		return err
+	}
+	defer ingressWatch.Stop()
+
+	serviceWatch, err := clients.CoreV1().Services("").Watch(emptyOptions)
+	if err != nil {
+		return err
+	}
+	defer serviceWatch.Stop()
+
+	endpointWatch, err := clients.CoreV1().Endpoints("").Watch(emptyOptions)
+	if err != nil {
+		return err
+	}
+	defer endpointWatch.Stop()
+
+	var event watch.Event
+	for {
+		select {
+		case event = <-ingressWatch.ResultChan():
+		case event = <-serviceWatch.ResultChan():
+		case event = <-endpointWatch.ResultChan():
+			// ignore endpoint events selected namespaces, because noise. See e.g.:
+			// https://github.com/kubernetes/kubernetes/issues/34627
+			ev, ok := event.Object.(*v13.Endpoints)
+			if ok {
+				if _, ok := config.IgnoreNamespaces[ev.Namespace]; ok {
+					log.Debug("Ignored kubernetes endpoint-API event",
+						zap.String("type", string(event.Type)),
+						zap.String("namespace", ev.Namespace),
+						zap.String("name", ev.Name),
+					)
+					continue
+				}
+			}
+		}
+		log.Debug("Received kubernetes API event",
+			zap.String("type", string(event.Type)),
+			zap.String("object", fmt.Sprint(event.Object)),
+		)
+		updateChan <- util.NewReload("api-change")
+	}
+}
+
 func UpdateBackends(config *util.Config) (map[string][]util.Backend, error) {
+	updateMutex.Lock()
+	defer updateMutex.Unlock()
 
 	clients, err := GetKubeClient(config.Kubeconfig)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: Move "getIngresses" to a separate loop, since it doesn't need to be updated as often
 	ingresses, err := getIngresses(clients.ExtensionsV1beta1().Ingresses(""))
 	if err != nil {
 		return nil, err
