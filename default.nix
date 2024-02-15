c62677972c6e0722ce72f83c30c76dd116ea7294
Sarah Brofeldt <sarah@qtr.dk>
2024-02-15 10:26:50 +0100

    vendorHash, 23.11

--- default.nix
+++ default.nix
@@ -5,5 +5,5 @@
 pkgs.buildGoModule {
   name = "shelob";
   src = ./.;
-  vendorSha256 = "sha256-PHUjvHwO6UOdWxTavFIkBbm/F1MDlSk/YmSTf1xGKS4=";
+  vendorHash = "sha256-PHUjvHwO6UOdWxTavFIkBbm/F1MDlSk/YmSTf1xGKS4=";
 }
